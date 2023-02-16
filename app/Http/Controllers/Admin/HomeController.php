cfafde8cb20ca5bb975420ac4bc6cfe6285f3028
your name <119037864+ht18boynd@users.noreply.github.com>
2023-02-16 21:44:31 +0700

    Update HomeController.php

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,18 +3,26 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Order;
+use App\Models\OrderDetail;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
+
         return view ('admin.homedb');
     }
 
     public function homedb()
     {
-        return view ('admin.homedb');
+        $prods = Product::all();
+        $order = Order::all();
+        $user = User::all();
+        return view ('admin.homedb',compact('prods' , 'order','user'));
     }
 
 }
