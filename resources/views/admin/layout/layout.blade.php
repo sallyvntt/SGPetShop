7b2cce239431a6f49137dc51e5d24fb9a3f6081d
your name <119037864+ht18boynd@users.noreply.github.com>
2023-02-16 21:44:53 +0700

    Update layout.blade.php

--- resources/views/admin/layout/layout.blade.php
+++ resources/views/admin/layout/layout.blade.php
@@ -157,7 +157,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{ Route('admin.home') }}" class="brand-link">
+    <a href="{{ Route('admin.homedb') }}" class="brand-link">
       <img src="{{ asset('/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">My Admin</span>
     </a>
