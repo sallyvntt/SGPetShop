101d1f84cbdfe2b86e12c949d09e48eb8a0e69f0
your name <119037864+ht18boynd@users.noreply.github.com>
2023-02-16 22:54:33 +0700

    Update edit.blade.php

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -9,7 +9,7 @@
       </div>
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
-          <li class="breadcrumb-item"><a href="{{ Route('admin.home') }}">Home</a></li>
+          <li class="breadcrumb-item"><a href="{{ Route('admin.homedb') }}">Home</a></li>
           <li class="breadcrumb-item"><a href="{{ Route('admin.user.index') }}">Users</a></li>
           <li class="breadcrumb-item active">Edit User</li>
         </ol>
