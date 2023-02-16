bf5be4dabceeeb1efb0a323e1e0c2d3ccbdd19b1
your name <119037864+ht18boynd@users.noreply.github.com>
2023-02-16 22:54:23 +0700

    Update create.blade.php

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -9,7 +9,7 @@
       </div>
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
-          <li class="breadcrumb-item"><a href="{{ Route('admin.home') }}">Home</a></li>
+          <li class="breadcrumb-item"><a href="{{ Route('admin.homedb') }}">Home</a></li>
           <li class="breadcrumb-item"><a href="{{ Route('admin.product.index') }}">Users</a></li>
           <li class="breadcrumb-item active">Create new Product</li>
         </ol>
