bd9aa45962b89b1e0f2e76bff0b58450cd19e008
your name <119037864+ht18boynd@users.noreply.github.com>
2023-02-17 11:08:01 +0700

    Update index.blade.php

--- resources/views/fe/index.blade.php
+++ resources/views/fe/index.blade.php
@@ -6,16 +6,16 @@
   <div class="wrap-main-slide">
     <div class="slide-carousel owl-carousel style-nav-1" data-items="1" data-loop="1" data-nav="true" data-dots="false">
       <div class="item-slide">
-        <img src="assets/images/main-slider-1-1.jpg" alt="" class="img-slide">
+        <img src="assets/images/cat.png" alt="" class="img-slide">
         <div class="slide-info slide-1">
-          <h2 class="f-title">Kid Smart <b>Watches</b></h2>
-          <span class="subtitle">Compra todos tus productos Smart por internet.</span>
-          <p class="sale-info">Only price: <span class="price">$59.99</span></p>
-          <a href="#" class="btn-link">Shop Now</a>
+          <!-- <h2 class="f-title">Kid Smart <b>Watches</b></h2> -->
+          <!-- <span class="subtitle">Compra todos tus productos Smart por internet.</span>
+          <p class="sale-info">Only price: <span class="price">$59.99</span></p> -->
+          <a href="{{Route('shop')}}" class="btn-link">Shop Now</a>
         </div>
       </div>
       <div class="item-slide">
-        <img src="assets/images/main-slider-1-2.jpg" alt="" class="img-slide">
+        <img src="assets/images/cat1.png" alt="" class="img-slide">
         <div class="slide-info slide-2">
           <h2 class="f-title">Extra 25% Off</h2>
           <span class="f-subtitle">On online payments</span>
@@ -24,7 +24,7 @@
           <p class="s-subtitle">TRansparent Bra Straps</p>
         </div>
       </div>
-      <div class="item-slide">
+      <!-- <div class="item-slide">
         <img src="assets/images/main-slider-1-3.jpg" alt="" class="img-slide">
         <div class="slide-info slide-3">
           <h2 class="f-title">Great Range of <b>Exclusive Furniture Packages</b></h2>
@@ -32,7 +32,7 @@
           <p class="sale-info">Stating at: <b class="price">$225.00</b></p>
           <a href="#" class="btn-link">Shop Now</a>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 
@@ -40,12 +40,13 @@
   <div class="wrap-banner style-twin-default">
     <div class="banner-item">
       <a href="#" class="link-banner banner-effect-1">
-        <figure><img src="assets/images/home-1-banner-1.jpg" alt="" width="580" height="190"></figure>
+        <figure><img src="assets/images/dichvu1.png" alt=""  style="height:270px"></figure>
       </a>
     </div>
     <div class="banner-item">
       <a href="#" class="link-banner banner-effect-1">
-        <figure><img src="assets/images/home-1-banner-2.jpg" alt="" width="580" height="190"></figure>
+        <figure><img src="assets/images/dichvu.png" alt=""   style="height:270px"></figure>
+
       </a>
     </div>
   </div>
