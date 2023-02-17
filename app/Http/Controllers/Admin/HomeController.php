a1e5f14b957c7f3197d5b7702319661985ebe119
your name <119037864+ht18boynd@users.noreply.github.com>
2023-02-17 11:07:18 +0700

    Update HomeController.php

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -11,12 +11,6 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
-    {
-
-        return view ('admin.homedb');
-    }
-
     public function homedb()
     {
         $prods = Product::all();
